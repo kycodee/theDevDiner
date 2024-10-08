779ccc7b0bafe719c8ca37133af279e7bb5cd1dd
kycodee <ky.patton87@gmail.com>
2024-10-08 18:08:39 -0500

    Imported axios into all tsx files and created onChange function for dayOfTheWeek dropdown

--- frontend/src/DinerDishInput.tsx
+++ frontend/src/DinerDishInput.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 
 function DinerDishInput() {
+
+
+    const [dayOfWeek, setDayOfWeek] = useState('')
+
+
+    function dayOfTheWeekOnChange(e: any) {
+        setDayOfWeek(e.value)
+    }
+
     return (
         <div className='d-flex justify-content-center'>
             <InputGroup className="w-50">
-                <Form.Select aria-label="Default select example">
+                <Form.Select aria-label="Default select example" onChange={(e) => dayOfTheWeekOnChange(e.target)}>
                     <option>Day of the Week</option>
-                    <option value="1">Monday</option>
-                    <option value="2">Tuesday</option>
-                    <option value="3">Wednesday</option>
-                    <option value="3">Thursday</option>
-                    <option value="3">Friday</option>
+                    <option value="Monday">Monday</option>
+                    <option value="Tuesday">Tuesday</option>
+                    <option value="Wednesday">Wednesday</option>
+                    <option value="Thursday">Thursday</option>
+                    <option value="Friday">Friday</option>
                 </Form.Select>
                 <Form.Control placeholder='What would you like your lunch to be?' />
                 <Button style={{backgroundColor: 'blue'}}>Submit</Button>
