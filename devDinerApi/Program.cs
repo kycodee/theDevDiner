39a06756141c8466cefbf6fe725ac90dc1942d7b
kycodee <ky.patton87@gmail.com>
2024-10-08 19:19:11 -0500

    Dishes are now successfully being added to database on submit click

--- devDinerApi/Program.cs
+++ devDinerApi/Program.cs
@@ -1,60 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using devDinerApi.Models;
-
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddControllers();
-// builder.Services.AddDbContext<DinerContext>(opt =>
-//     opt.UseInMemoryDatabase("DinerMenu"));
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
-
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-
-// app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register services with the DI container
+// builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod() // Allows GET, POST, PUT, DELETE
+                          .AllowAnyHeader()); // Allows any headers
+});
 builder.Services.AddControllers();
 // builder.Services.AddDbContext<DinerContext>(opt =>
 //     opt.UseInMemoryDatabase("DinerMenu"));
@@ -65,15 +26,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseRouting();
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 
 // Map the controllers
 app.MapControllers();
--- frontend/src/DinerDishInput.tsx
+++ frontend/src/DinerDishInput.tsx
@@ -6,16 +6,34 @@ function DinerDishInput() {
 
 
     const [dayOfWeek, setDayOfWeek] = useState('')
+    const [dishName, setDishName] = useState('')
+
+    function postDishOnSubmit() {
+        axios.post('http://localhost:5044/api/Diner/postDinerDish', {
+                dishName,
+                dayOfWeek
+        })
+            .then(() => {
+                console.log('post successful')
+            })
+            .catch((err: string) => {
+                console.error(err);
+              });
+    }
 
 
-    function dayOfTheWeekOnChange(e: any) {
+    function updateDishNameOnChange(e: any) {
+        setDishName(e)
+    }
+
+    function updateDayOfTheWeekOnChange(e: any) {
         setDayOfWeek(e.value)
     }
 
     return (
         <div className='d-flex justify-content-center'>
             <InputGroup className="w-50">
-                <Form.Select aria-label="Default select example" onChange={(e) => dayOfTheWeekOnChange(e.target)}>
+                <Form.Select aria-label="Default select example" onChange={(e) => updateDayOfTheWeekOnChange(e.target)}>
                     <option>Day of the Week</option>
                     <option value="Monday">Monday</option>
                     <option value="Tuesday">Tuesday</option>
@@ -23,8 +41,8 @@ function DinerDishInput() {
                     <option value="Thursday">Thursday</option>
                     <option value="Friday">Friday</option>
                 </Form.Select>
-                <Form.Control placeholder='What would you like your lunch to be?' />
-                <Button style={{backgroundColor: 'blue'}}>Submit</Button>
+                <Form.Control placeholder='What would you like your lunch to be?' onChange={(e) => updateDishNameOnChange(e.target.value)} />
+                <Button style={{ backgroundColor: 'blue' }} onClick={postDishOnSubmit}>Submit</Button>
             </InputGroup>
         </div>
     )
